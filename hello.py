0d653b9ddb1474461b8cd4d5a98f4965676a6e96
akhil <sharma.akhil484@gmail.com>
2019-11-18 13:18:46 +0530

    changes function

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
 import itertools
-from flask import *
-app=Flask(__name__)
-@app.route('/')
 
 class tictactoe():
-	game=[[0,0,0],
-		  [0,0,0],
-		  [0,0,0]]
+ 
+	def __init__(self):
+		self.game=[[0,0,0],
+			       [0,0,0],
+			       [0,0,0]]
+		self.check=False
+		self.flag=0
+
+
+	def display(self,msg):
+		if(msg=="winner"):
+			print("We have a winner!!!!")
+		elif(msg=="filled"):
+			print("This is position is occupied!!!!!!!. Try another")
+		else:
+			print("Something went wrong",msg)
+		
+
 
 	def win(self):
 		def all(l):
@@ -16,26 +28,34 @@ class tictactoe():
 				return False
 		# Horizontal
 		for row in self.game:
-			print(row)
+			#print(row)
 			if all(row):
-				print("Player",row[0],"is the winner horizontally!")
+				#print("Player",row[0],"is the winner horizontally!")
+				self.display("winner")
+				self.flag=1
 				return True
+			
 
 		# / Diagonal
 		diag=[]
 		for x,y in enumerate(reversed(range(len(self.game)))):
 			diag.append(self.game[y][x])
 		if all(diag):
-				print("Player",diag[0],"is the winner Diagonally(/)!")
-				return True
+			self.display("winner")
+				#print("Player",diag[0],"is the winner Diagonally(/)!")
+			self.flag=1
+			return True
 
 		# \ Diagonal
 		diag=[]
 		for z in range(len(self.game)):
 			diag.append(self.game[z][z])
 		if all(diag):
-				print("Player",diag[0],"is the winner Diagonally(\\)!")
-				return True
+			self.display("winner")
+			self.flag=1
+				#print("Player",diag[0],"is the winner Diagonally(\\)!")
+			return True
+	
 
 		# Vertical
 		for col in range(len(self.game)):
@@ -44,44 +64,60 @@ class tictactoe():
 			for row in self.game:
 				check.append(row[col])
 			if all(check):
-				print("Player",check[0],"is the winner Vertically!")  
-				return True 
+				self.display("winner")
+				self.flag=1
+				#print("Player",check[0],"is the winner Vertically!")  
+				return True
+		if(self.flag==0):
+			return False
 
 
-	
-	def game_board(self,player=0, row=0, column=0,just_display=False):
+
+	def print_gameboard(self):
+		print ("   "+"  ".join([str(i) for i in range(len(self.game))]))
+		for count,row in enumerate(self.game):
+
+			print (count,row)
+
+
+
+	def game_board(self,row=0, column=0,just_display=False):
 		try:
 			if self.game[row][column]!=0:
-				print("This is position is occupied!!!!!!!. Try another")
-				return False
-			print ("   "+"  ".join([str(i) for i in range(len(self.game))]))
+				self.display("filled")
+				#print("This is position is occupied!!!!!!!. Try another")
+				self.check = False
 			if not just_display:
-				self.game[row][column]=player
-			for count,row in enumerate(self.game):
-				print (count,row)
-			return True
+				self.game[row][column]=1
+			self.print_gameboard()
+			#return True
+			self.check = self.win()
 		except Exception as e:
-			print("Something went wrong",e)
-			return False
-		except IndexError as e:
-			print("You might have entered position other than 0 1 or 2", e)
-			return False  
+			self.display(e);
+			self.print_gameboard()
+			self.check = False
+
+
+	def chec(self):
+		return self.check
 
 
 g=tictactoe()
-players_choice=itertools.cycle([1,2])
-check=False
-while not check:
-	played=False
-	while not played:
-		current_player=next(players_choice)
-		row_choice=int(input("Which row do you want to choose?(0,1,2): "))
-		column_choice=int(input("Which column do you want to choose?(0,1,2): "))
-		played=g.game_board(current_player,row_choice,column_choice)
-		check=g.win()
+while g.chec()==False:
+	row_choice=int(input("Which row do you want to choose?(0,1,2): "))
+	column_choice=int(input("Which column do you want to choose?(0,1,2): "))
+	g.game_board(row_choice,column_choice)
 
 
 
+"""class Bot(tictactoe):
 
+	def __init__(self):
+		super(self)
+
+	def bot()
+		self.game
+		self.player_choice
+		# bot logic <- random """
 
 
